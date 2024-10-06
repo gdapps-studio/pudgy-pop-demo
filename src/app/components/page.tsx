1146bcb92db23fa8a467fafe8207c7c08a6c5731
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-10-06 09:14:29 +0200

    fix mobile overflow

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -33,7 +33,7 @@ const Section = ({ children, title }: { children: ReactNode; title: string | und
 
 export default function ComponentPage() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen space-y-6 relative overflow-hidden">
+    <main className="flex flex-col justify-center items-center space-y-6">
       <Section title="Button Variations">
         <div className="flex flex-col md:flex-row gap-5">
           <Btn1>Button 1</Btn1>
