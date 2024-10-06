2132265d215e8d7f3f6d0a15794cfe58eed813ca
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-10-06 17:59:19 +0200

    background image div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
+    <main className="flex flex-col justify-center items-center">
       <Link href="/components" className="flex items-center group">
         Go to components
         <span className="ml-2 transition-transform duration-300 ease-in-out transform group-hover:translate-x-2">
